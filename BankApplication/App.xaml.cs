123d32fddf6e02ce3cc89e6647f9b6abaeda754a
artem.denisoff <tema.deniosff@gmail.com>
2020-08-16 13:09:58 +0300

    add theme

--- BankApplication/App.xaml.cs
+++ BankApplication/App.xaml.cs
@@ -1,6 +1,15 @@
-﻿namespace BankApplication {
+﻿using DevExpress.Xpf.Core;
+using System.Windows;
+
+namespace BankApplication {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App { }
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ApplicationThemeHelper.ApplicationThemeName = Theme.Office2016ColorfulName;
+            base.OnStartup(e);
+        }
+    }
 }
